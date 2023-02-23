d244a1fc4e91831742dc221dde03d3361d284b82
Ali-Ayb <aliayoub.me1@gmail.com>
2023-02-24 01:06:56 +0200

    changed the block dragging from on click to hover (onmouseover)

--- game.js
+++ game.js
@@ -1,3 +1,83 @@
+// document.addEventListener("DOMContentLoaded", function (event) {
+//   score = 0;
+//   var scoreDisplay = document.createElement("p");
+//   scoreDisplay.textContent = "Score: " + score;
+//   document.body.appendChild(scoreDisplay);
+//   dragElement(document.getElementById("start"));
+
+//   function dragElement(elmnt) {
+//     var pos1 = 0,
+//       pos2 = 0,
+//       pos3 = 0,
+//       pos4 = 0;
+
+//     document.getElementById("start").onmousedown = dragMouseDown;
+
+//     function dragMouseDown(element) {
+//       element.preventDefault();
+//       startx = pos3 = element.clientX;
+//       starty = pos4 = element.clientY;
+//       document.onmouseup = closeDragElement;
+//       document.onmousemove = elementDrag;
+//     }
+
+//     function elementDrag(element) {
+//       element.preventDefault();
+//       pos1 = pos3 - element.clientX;
+//       pos2 = pos4 - element.clientY;
+//       pos3 = element.clientX;
+//       pos4 = element.clientY;
+
+//       elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+//       elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+//       if (checkCollisions()) {
+//         document.getElementById("status").textContent = "You lose!";
+//       }
+//       console.log(score);
+//     }
+
+//     function closeDragElement() {
+//       document.onmouseup = null;
+//       document.onmousemove = null;
+//     }
+
+//     function checkCollisions() {
+//       var boundaries = document.querySelectorAll(".boundary");
+//       var collided = false;
+//       for (var i = 0; i < boundaries.length; i++) {
+//         var boundary = boundaries[i];
+//         if (isColliding(elmnt, boundary)) {
+//           collided = true;
+//           break;
+//         }
+//       }
+//       if (collided) {
+//         document.getElementById("status").textContent = "You lose! :(";
+//         closeDragElement();
+//         document.getElementById("game").classList.add("youlose");
+//         score -= 10;
+//         scoreDisplay.textContent = "Score: " + score;
+//       } else if (isColliding(elmnt, document.getElementById("end"))) {
+//         document.getElementById("status").textContent = "You win! :)";
+//         document.getElementById("game").classList.add("youwon");
+//         score += 5;
+//         scoreDisplay.textContent = "Score: " + score;
+//         closeDragElement();
+//       }
+//       function isColliding(a, b) {
+//         var aRect = a.getBoundingClientRect();
+//         var bRect = b.getBoundingClientRect();
+//         return !(
+//           aRect.bottom < bRect.top ||
+//           aRect.top > bRect.bottom ||
+//           aRect.right < bRect.left ||
+//           aRect.left > bRect.right
+//         );
+//       }
+//     }
+//   }
+// });
+
 document.addEventListener("DOMContentLoaded", function (event) {
   score = 0;
   var scoreDisplay = document.createElement("p");
@@ -11,13 +91,13 @@ document.addEventListener("DOMContentLoaded", function (event) {
       pos3 = 0,
       pos4 = 0;
 
-    document.getElementById("start").onmousedown = dragMouseDown;
+    document.getElementById("start").onmouseover = dragMouseOver;
 
-    function dragMouseDown(element) {
+    function dragMouseOver(element) {
       element.preventDefault();
       startx = pos3 = element.clientX;
       starty = pos4 = element.clientY;
-      document.onmouseup = closeDragElement;
+      //   document.onmouseup = closeDragElement;
       document.onmousemove = elementDrag;
     }
 
